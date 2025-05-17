74ae5c6c900a8a35061a1335e4ca1fc766e97612
JavierMartinezAraya <jjavierrmartinezz@gmail.com>
2025-05-17 18:48:45 -0400

    PruebaSFML
    Probando SFML

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
 
 int main(){
-    std::cout<<"Hola cambio"<< std::endl;
-    std::cout<<"Testeo 2"<< std::endl;
-    std::cout<<"Testeo 4"<< std::endl;
+
+        sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    //std::cout<<"Hola cambio"<< std::endl;
+    //std::cout<<"Testeo 2"<< std::endl;
+    //std::cout<<"Testeo 4"<< std::endl;
 
 
     // prueba de cambio 1
-    return 0;
+    //return 0;
     
 }
